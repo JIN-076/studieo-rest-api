a9317c92639bb79a59c65d08b898ded3a4e5468c
JIN-076 <andy230@naver.com>
2023-11-14 21:32:02 +0900

    init: 초기 프로젝트 세팅

---
+++ studieo-rest-api/src/main/java/org/dev/course/studieorestapi/StudieoRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.dev.course.studieorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudieoRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudieoRestApiApplication.class, args);
+    }
+
+}
