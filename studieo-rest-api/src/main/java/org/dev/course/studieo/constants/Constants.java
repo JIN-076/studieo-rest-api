8a3d72d77b2d564124f71faec2a42647137a6227
JIN-076 <andy230@naver.com>
2023-11-21 18:35:25 +0900

    feat: App을 아우르는 Constants 클래스 추가. 인터페이스를 이용해 인스턴스화 방지

---
+++ studieo-rest-api/src/main/java/org/dev/course/studieo/constants/Constants.java
@@ -0,0 +1,13 @@
+package org.dev.course.studieo.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Constants {
+
+    String SESSION_KEY = "loginId";
+    Integer NONE = 0;
+    Integer isBelong = 1;
+    Integer isLeader = 1;
+    Integer isApply = 1;
+}
