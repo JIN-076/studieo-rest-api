71451845d86184781113bdde8d66f458c272d0a0
JIN-076 <andy230@naver.com>
2023-11-21 17:47:10 +0900

    feat: UUID, bytes[] 간의 변환을 담당하는 Converter 클래스 생성

---
+++ studieo-rest-api/src/main/java/org/dev/course/studieo/util/converter/UUIDConverter.java
@@ -0,0 +1,18 @@
+package org.dev.course.studieo.util.converter;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public class UUIDConverter {
+
+    private UUIDConverter() {}
+
+    public static UUID toUUID(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    public static byte[] toBytes(UUID uuid) {
+        return uuid.toString().getBytes();
+    }
+}
